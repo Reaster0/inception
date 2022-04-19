81952f24e43fd82359403e7554d5711464072a6c
Reaster <earnaud@student.42.fr>
2022-04-19 18:11:58 +0200

    let's gooooo all working

--- mariadb/docker-entrypoint.sh
+++ mariadb/docker-entrypoint.sh
@@ -1,12 +1,15 @@
 #bin/sh
 
-# sed -i -e "s/\${MYSQL_USER}/${MYSQL_USER}/g" /etc/my.cnf;
-# sed -i -e "s/\${MYSQL_PASSWORD}/${MYSQL_PASSWORD}/g" /etc/my.cnf;
 service mysql start
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'';"
-mysql -e "CREATE DATABASE IF NOT EXISTS '$MYSQL_DATABASE'' ;"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
-mysql -e "FLUSH PRIVILEGES;"
+#tail -f /dev/null
+echo "setup mysql ❇️"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+#mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_ADMIN'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';"
+mysql -u root -e "USE $MYSQL_DATABASE; GRANT ALL PRIVILEGES ON * TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
+#mysql -u root -e "USE $MYSQL_DATABASE; GRANT ALL PRIVILEGES ON * TO '$MYSQL_ADMIN'@'%' WITH GRANT OPTION;"
+mysql -u root -e "FLUSH PRIVILEGES;"
 service mysql stop
-mysqld
+echo "starting mysql ✅"
+mysqld_safe
 echo "is not okay?"
