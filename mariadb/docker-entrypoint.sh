46923c0e93d311032b7600cf356938d980b5954c
Reaster <earnaud@student.42.fr>
2022-04-22 21:12:36 +0200

    going to make wp-cli to work

--- mariadb/docker-entrypoint.sh
+++ mariadb/docker-entrypoint.sh
@@ -1,10 +1,11 @@
 #bin/sh
 service mysql start
-sleep 3
+sleep 1
 echo "setup mysql ❇️"
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;"
 mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mysql -u root -e "USE $MYSQL_DATABASE; GRANT ALL PRIVILEGES ON * TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
+#mysql -u root -e "SET PASSWORD 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
 mysql -u root -e "FLUSH PRIVILEGES;"
 service mysql stop
 echo "starting mysql ✅"
