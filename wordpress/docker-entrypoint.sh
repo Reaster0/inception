fac3744572c0f6ca0fef1c95a6ab2ca850c8b5c1
Reaster <earnaud@student.42.fr>
2022-04-24 15:02:26 +0200

    going back in time

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -11,6 +11,6 @@ sleep 5
 wp core install --allow-root --path=/wordpress --url="earnaud.42.fr" --title="My website" --admin_user="earnaud" --admin_password="1234" --admin_email="earnaud@student.42.fr" --skip-email
 #cd wordpress && wp user create --allow-root user2 earnaud@42.fr --user_pass=user2
 cp -rf /wordpress/* /var/www/html/
-#php-fpm7.3 --nodaemonize
 echo "running php-fpm ✅"
-service php7.3-fpm start
+php-fpm7 --nodaemonize
+#service php7.3-fpm start
